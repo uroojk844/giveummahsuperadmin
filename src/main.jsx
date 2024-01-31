afe22f60ccf28033a548ecdd8df36bd406b7c353
urooj khan <uroojk844@gmail.comm>
2024-01-31 12:27:11 +0530

    fix: resolved conflict in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
-<<<<<<< HEAD
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-=======
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
->>>>>>> 9c782e8087b3f5a418b261faee4bcab921130ebf
